4124bce4d3c4e9cc8a2a9fad4b5e6bc0c64f0650
Jonas Bergert <jonas@bergert.digital>
2026-05-19 12:14:33 +0200

    feat(theme): mega-menu defaults to 1 column + Add column button

--- src/blocks/mega-menu/edit.tsx
+++ src/blocks/mega-menu/edit.tsx
@@ -3,12 +3,14 @@ import {
 	useBlockProps,
 	useInnerBlocksProps,
 	RichText,
+	store as blockEditorStore,
 } from '@wordpress/block-editor';
+import { useDispatch } from '@wordpress/data';
+import { createBlock } from '@wordpress/blocks';
+import { Button } from '@wordpress/components';
 
 const ALLOWED = [ 'starter/mega-column' ];
 const TEMPLATE: [ string, Record< string, unknown >, unknown[] ][] = [
-	[ 'starter/mega-column', {}, [] ],
-	[ 'starter/mega-column', {}, [] ],
 	[ 'starter/mega-column', {}, [] ],
 ];
 
@@ -17,14 +19,37 @@ type Attrs = { label: string };
 export default function Edit( {
 	attributes,
 	setAttributes,
+	clientId,
 }: {
 	attributes: Attrs;
 	setAttributes: ( a: Partial< Attrs > ) => void;
+	clientId: string;
 } ) {
 	const blockProps = useBlockProps( { className: 'starter-mega-menu' } );
+	const { insertBlock } = useDispatch( blockEditorStore );
+	const addColumn = () =>
+		insertBlock(
+			createBlock( 'starter/mega-column' ),
+			undefined,
+			clientId
+		);
 	const innerBlocksProps = useInnerBlocksProps(
 		{ className: 'starter-mega-menu__panel' },
-		{ allowedBlocks: ALLOWED, template: TEMPLATE, templateLock: false }
+		{
+			allowedBlocks: ALLOWED,
+			template: TEMPLATE,
+			templateLock: false,
+			renderAppender: () => (
+				<Button
+					variant="secondary"
+					icon="plus"
+					className="starter-mega-menu__add-col"
+					onClick={ addColumn }
+				>
+					{ __( 'Add column', 'starter' ) }
+				</Button>
+			),
+		}
 	);
 	return (
 		<div { ...blockProps }>
